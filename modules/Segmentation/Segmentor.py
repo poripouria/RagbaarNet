e031a7c77f061bf835d1613fbf3c90a66191582e
Poripouria <poripouria9@gmail.com>
2026-07-02 00:41:07 +0330

    remove unused set_level import

--- modules/Segmentation/Segmentor.py
+++ modules/Segmentation/Segmentor.py
@@ -18,7 +18,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from utils.logging_setup import setup_logging, set_level
+from utils.logging_setup import setup_logging
 
 logger = setup_logging("INFO", name="segmentation.segmentor")
 
