66671ba865a726c8e31bd3dd9c9f2d773cec212e
Poripouria <poripouria9@gmail.com>
2026-08-10 13:35:21 +0330

    return back the frame sending interval

--- modules/Platform/js/video-pipeline.js
+++ modules/Platform/js/video-pipeline.js
@@ -42,7 +42,7 @@ let frameCounter = 0;
 let lastFrameSentTime = 0;
 
 // Adapt frame send rate on mobile to reduce bandwidth/CPU contention
-let frameSendInterval = isMobileDevice() ? 200 : 100;
+let frameSendInterval = isMobileDevice() ? 250 : 150;
 
 // ms
 let processingCanvas = null;
